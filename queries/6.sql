84db21c274ce1a3a46f60ca348a9869dcd3638dd
Priyanka Lakur Krishnamurthy <67971841+Priyanka-L-K@users.noreply.github.com>
2025-02-20 14:18:05 -0600

    Create 6.sql

---
+++ queries/6.sql
@@ -0,0 +1,6 @@
+SELECT s.customer_id, m.product_name
+FROM sales s
+JOIN menu m ON s.product_id = m.product_id
+JOIN members mem ON s.customer_id = mem.customer_id
+WHERE s.order_date >= mem.join_date
+AND s.order_date = (SELECT MIN(order_date) FROM sales s2 WHERE s2.customer_id = s.customer_id AND s2.order_date >= mem.join_date);
